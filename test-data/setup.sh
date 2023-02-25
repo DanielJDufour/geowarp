e25918b39663241a0700f9cf72004e4486fb0600
DanielJDufour <daniel.j.dufour@gmail.com>
2023-02-25 09:56:42 -0500

    updated test-data script to include planet COG

--- test-data/setup.sh
+++ test-data/setup.sh
@@ -5,6 +5,8 @@ wget https://github.com/GeoTIFF/test-data/archive/refs/heads/main.zip -O geotiff
 unzip -j -o geotiff-test-data.zip "test-data-*/files/*" -d .
 rm geotiff-test-data.zip
 
+wget https://storage.googleapis.com/pdd-stac/disasters/hurricane-harvey/0831/SkySat_Freeport_s03_20170831T162740Z.tif
+
 wget https://raw.githubusercontent.com/GeoTIFF/georaster-layer-for-leaflet-example/master/example_4326.tif
 
 wget https://georaster-layer-for-leaflet.s3.amazonaws.com/check.tif
