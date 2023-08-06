632631830dff1f9019d3feff264f0c7422cf3687
DanielJDufour <daniel.j.dufour@gmail.com>
2023-08-06 12:41:53 -0400

    updated test data

--- test.ts
+++ test.ts
@@ -377,7 +377,8 @@ const runTileTests = async ({
       "27,35,22",
       "36,46,45",
       "40,49,47",
-      "42,51,48"
+      "42,51,48",
+      "46,54,48"
     ],
     turbos: [false, true],
     out_resolutions: [
